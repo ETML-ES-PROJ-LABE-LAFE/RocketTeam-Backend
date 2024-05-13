0fa0d97cc9f464930effb23f2285633eabd3961f
OhceZA <az.echotwitch@gmail.com>
2024-05-13 12:17:04 +0200

    f_insertdonnees : Insertion de données de test pour les lots

--- src/main/java/ch/etmles/payroll/Repositories/LoadDatabase.java
+++ src/main/java/ch/etmles/payroll/Repositories/LoadDatabase.java
@@ -16,18 +16,18 @@ public class LoadDatabase {
     CommandLineRunner initDatabase(CategoryRepository categoryRepository, LotRepository lotRepository) {
         return args -> {
             // Création et sauvegarde des catégories
-            Category mainCategory1 = categoryRepository.save(new Category("Main Category 1", null));
-            Category mainCategory2 = categoryRepository.save(new Category("Main Category 2", null));
+            Category mainCategory1 = categoryRepository.save(new Category("Vêtements", null));
+            Category mainCategory2 = categoryRepository.save(new Category("Meubles", null));
 
-            Category subCategory1 = categoryRepository.save(new Category("Subcategory 1", mainCategory1));
-            Category subCategory2 = categoryRepository.save(new Category("Subcategory 2", mainCategory1));
-            Category subCategory3 = categoryRepository.save(new Category("Subcategory 3", mainCategory2));
+            Category subCategory1 = categoryRepository.save(new Category("Hauts", mainCategory1));
+            Category subCategory2 = categoryRepository.save(new Category("Bas", mainCategory1));
+            Category subCategory3 = categoryRepository.save(new Category("Chambre", mainCategory2));
 
             // Création et sauvegarde des lots avec leurs sous-catégories
-            Lot lot1 = lotRepository.save(new Lot("Lot description 1", subCategory1, 100.0, 150.0));
-            Lot lot2 = lotRepository.save(new Lot("Lot description 2", subCategory1, 200.0, 250.0));
-            Lot lot3 = lotRepository.save(new Lot("Lot description 3", subCategory2, 300.0, 350.0));
-            Lot lot4 = lotRepository.save(new Lot("Lot description 4", subCategory3, 400.0, 450.0));
+            Lot lot1 = lotRepository.save(new Lot("Pull Gucci", subCategory1, 100.0, 150.0));
+            Lot lot2 = lotRepository.save(new Lot("T-Shirt Ralph Lauren", subCategory1, 200.0, 250.0));
+            Lot lot3 = lotRepository.save(new Lot("Pantalon Jack&Jones", subCategory2, 300.0, 350.0));
+            Lot lot4 = lotRepository.save(new Lot("Lit Cars", subCategory3, 400.0, 450.0));
 
             // Vous pouvez ajouter plus de catégories, sous-catégories et lots ici
         };
