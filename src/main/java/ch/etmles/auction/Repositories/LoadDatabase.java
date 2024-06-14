bdbd1d236e84ce1be5c77eabd5a611774b54e8ed
OhceZA <az.echotwitch@gmail.com>
2024-06-14 13:25:39 +0200

    feat : Pull images from s3 server

--- src/main/java/ch/etmles/auction/Repositories/LoadDatabase.java
+++ src/main/java/ch/etmles/auction/Repositories/LoadDatabase.java
@@ -6,6 +6,7 @@ import ch.etmles.auction.Entities.Lot;
 import ch.etmles.auction.Entities.Customer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,9 @@ import java.time.LocalDateTime;
 @Configuration
 public class LoadDatabase {
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Value("${s3.bucket.url}")
+    private String s3BucketUrl;
 
     @Bean
     CommandLineRunner initDatabase(CategoryRepository categoryRepository, LotRepository lotRepository, CustomerRepository customerRepository, EnchereRepository enchereRepository) {
@@ -42,40 +46,40 @@ public class LoadDatabase {
             Category subCategory9 = categoryRepository.save(new Category("Appareils photo", mainCategory3));
 
             // Création et sauvegarde des lots avec leurs sous-catégories
-            Lot lot1 = lotRepository.save(new Lot("Pull Gucci", "pull_gucci.jpg", subCategory1, 100.0, 150.0, "active", Identify1));
-            Lot lot2 = lotRepository.save(new Lot("T-Shirt Ralph Lauren", "t-shirt_ralphlauren.jpg", subCategory1, 200.0, 250.0, "active", Identify2));
-            Lot lot3 = lotRepository.save(new Lot("Pantalon Jack&Jones", "pantalon_jackandjones.jpg", subCategory2, 300.0, 350.0, "active", Identify1));
-            Lot lot4 = lotRepository.save(new Lot("Lit Cars", "lit_cars.jpg", subCategory3, 400.0, 450.0, "active", Identify2));
-            Lot lot5 = lotRepository.save(new Lot("Canapé en cuir", "canape_cuir.jpg", subCategory4, 500.0, 550.0, "active", Identify1));
-            Lot lot6 = lotRepository.save(new Lot("Table basse moderne", "table_bassemoderne.jpg", subCategory4, 600.0, 650.0, "active", Identify2));
-            Lot lot7 = lotRepository.save(new Lot("Samsung 4K TV", "Samsung_tele.jpg", subCategory5, 700.0, 750.0, "active", Identify1));
-            Lot lot8 = lotRepository.save(new Lot("iPhone 12", "iphone12.jpg", subCategory6, 800.0, 850.0, "active", Identify2));
-            Lot lot9 = lotRepository.save(new Lot("Asus Zenbook Pro", "Asus_Zenbook.jpg", subCategory7, 900.0, 950.0, "active", Identify3));
-            Lot lot10 = lotRepository.save(new Lot("Chaise de bureau", "chaise_bureau.jpg", subCategory4, 1000.0, 1050.0, "active", Identify2));
-            Lot lot11 = lotRepository.save(new Lot("Sony 65 TV", "sony_65.jpg", subCategory5, 1100.0, 1150.0, "active", Identify1));
-            Lot lot12 = lotRepository.save(new Lot("HP Envy x360", "HP_Envy.jpg", subCategory7, 1200.0, 1250.0, "active", Identify2));
-            Lot lot13 = lotRepository.save(new Lot("Manteau en laine", "manteau_en_laine.jpg", subCategory1, 1300.0, 1350.0, "active", Identify1));
-            Lot lot14 = lotRepository.save(new Lot("Jeans Levi's", "jeans_levis.jpg", subCategory2, 1400.0, 1450.0, "active", Identify2));
-            Lot lot15 = lotRepository.save(new Lot("Lit enfant", "lit_enfant.jpg", subCategory3, 1500.0, 1550.0, "active", Identify3));
-            Lot lot16 = lotRepository.save(new Lot("Câble HDMI", "cable_hdmi.jpg", subCategory8, 10.0, 15.0, "active", Identify1));
-            Lot lot17 = lotRepository.save(new Lot("Chargeur USB-C", "USB_C.jpg", subCategory8, 5.0, 10.0, "active", Identify3));
-            Lot lot18 = lotRepository.save(new Lot("Canon EOS 80D", "canon_eos.jpg", subCategory9, 900.0, 950.0, "active", Identify1));
-            Lot lot19 = lotRepository.save(new Lot("Nikon D750", "nikon_d750.jpg", subCategory9, 1000.0, 1050.0, "active", Identify2));
-            Lot lot20 = lotRepository.save(new Lot("Étui pour iPhone", "etui_telephone.jpg", subCategory8, 15.0, 20.0, "active", Identify1));
-            Lot lot21 = lotRepository.save(new Lot("Objectif Sigma 35mm", "objectif_sigma.jpg", subCategory9, 600.0, 650.0, "active", Identify3));
-            Lot lot22 = lotRepository.save(new Lot("Sacoche pour appareil photo", "sacoche_appareil_photo.jpg", subCategory8, 25.0, 30.0, "active", Identify1));
-            Lot lot23 = lotRepository.save(new Lot("Trépied Manfrotto", "trepied_manfrotto.jpg", subCategory9, 150.0, 200.0, "active", Identify3));
-            Lot lot24 = lotRepository.save(new Lot("Écouteurs sans fil", "ecouteur_sansfil.jpg", subCategory8, 50.0, 60.0, "active", Identify1));
-            Lot lot25 = lotRepository.save(new Lot("Stabilisateur DJI Ronin-S", "stabilisateur_ronin.jpg", subCategory9, 400.0, 450.0, "active", Identify2));
-            Lot lot26 = lotRepository.save(new Lot("GoPro Hero 9", "gopro_9.jpg",subCategory9, 350.0, 400.0, "active", Identify3));
-            Lot lot27 = lotRepository.save(new Lot("Étui pour GoPro", "etui_gopro.jpg", subCategory8, 20.0, 25.0, "active", Identify2));
-            Lot lot28 = lotRepository.save(new Lot("Écouteurs Apple AirPods", "AirPods.jpg", subCategory8, 150.0, 180.0, "active", Identify3));
-            Lot lot29 = lotRepository.save(new Lot("Carte mémoire SanDisk", "memorycard_sandisk.jpg", subCategory8, 30.0, 35.0, "active", Identify2));
-            Lot lot30 = lotRepository.save(new Lot("Gimbal pour smartphone", "gimbal_smartphone.jpg", subCategory9, 100.0, 120.0, "active", Identify1));
-            Lot lot31 = lotRepository.save(new Lot("Souris sans fil Logitech", "souris_logitech.jpg", subCategory8, 25.0, 30.0, "active", Identify1));
-            Lot lot32 = lotRepository.save(new Lot("Clé USB 64 Go", "usb_64.jpg", subCategory8, 20.0, 25.0, "active", Identify3));
-            Lot lot33 = lotRepository.save(new Lot("Batterie externe Anker 10000 mAh", "batterie_anker.jpg", subCategory8, 35.0, 40.0, "active", Identify1));
-            Lot lot34 = lotRepository.save(new Lot("Câble Lightning Apple", "lightning_apple.jpg", subCategory8, 15.0, 20.0, "active", Identify2));
+            Lot lot1 = lotRepository.save(new Lot("Pull Gucci",s3BucketUrl + "pull_gucci.jpg", subCategory1, 100.0, 150.0, "active", Identify1));
+            Lot lot2 = lotRepository.save(new Lot("T-Shirt Ralph Lauren", s3BucketUrl + "t-shirt_ralphlauren.jpg", subCategory1, 200.0, 250.0, "active", Identify2));
+            Lot lot3 = lotRepository.save(new Lot("Pantalon Jack&Jones", s3BucketUrl + "pantalon_jackandjones.jpg", subCategory2, 300.0, 350.0, "active", Identify1));
+            Lot lot4 = lotRepository.save(new Lot("Lit Cars", s3BucketUrl + "lit_cars.jpg", subCategory3, 400.0, 450.0, "active", Identify2));
+            Lot lot5 = lotRepository.save(new Lot("Canapé en cuir", s3BucketUrl + "canape_cuir.jpg", subCategory4, 500.0, 550.0, "active", Identify1));
+            Lot lot6 = lotRepository.save(new Lot("Table basse moderne", s3BucketUrl + "table_bassemoderne.jpg", subCategory4, 600.0, 650.0, "active", Identify2));
+            Lot lot7 = lotRepository.save(new Lot("Samsung 4K TV", s3BucketUrl + "Samsung_tele.jpg", subCategory5, 700.0, 750.0, "active", Identify1));
+            Lot lot8 = lotRepository.save(new Lot("iPhone 12", s3BucketUrl + "iphone12.jpg", subCategory6, 800.0, 850.0, "active", Identify2));
+            Lot lot9 = lotRepository.save(new Lot("Asus Zenbook Pro", s3BucketUrl + "Asus_Zenbook.jpg", subCategory7, 900.0, 950.0, "active", Identify3));
+            Lot lot10 = lotRepository.save(new Lot("Chaise de bureau", s3BucketUrl + "chaise_bureau.jpg", subCategory4, 1000.0, 1050.0, "active", Identify2));
+            Lot lot11 = lotRepository.save(new Lot("Sony 65 TV", s3BucketUrl + "sony_65.jpg", subCategory5, 1100.0, 1150.0, "active", Identify1));
+            Lot lot12 = lotRepository.save(new Lot("HP Envy x360", s3BucketUrl + "HP_Envy.jpg", subCategory7, 1200.0, 1250.0, "active", Identify2));
+            Lot lot13 = lotRepository.save(new Lot("Manteau en laine", s3BucketUrl + "manteau_en_laine.jpg", subCategory1, 1300.0, 1350.0, "active", Identify1));
+            Lot lot14 = lotRepository.save(new Lot("Jeans Levi's", s3BucketUrl + "jeans_levis.jpg", subCategory2, 1400.0, 1450.0, "active", Identify2));
+            Lot lot15 = lotRepository.save(new Lot("Lit enfant", s3BucketUrl + "lit_enfant.jpg", subCategory3, 1500.0, 1550.0, "active", Identify3));
+            Lot lot16 = lotRepository.save(new Lot("Câble HDMI", s3BucketUrl + "cable_hdmi.jpg", subCategory8, 10.0, 15.0, "active", Identify1));
+            Lot lot17 = lotRepository.save(new Lot("Chargeur USB-C", s3BucketUrl + "USB_C.jpg", subCategory8, 5.0, 10.0, "active", Identify3));
+            Lot lot18 = lotRepository.save(new Lot("Canon EOS 80D", s3BucketUrl + "canon_eos.jpg", subCategory9, 900.0, 950.0, "active", Identify1));
+            Lot lot19 = lotRepository.save(new Lot("Nikon D750", s3BucketUrl + "nikon_d750.jpg", subCategory9, 1000.0, 1050.0, "active", Identify2));
+            Lot lot20 = lotRepository.save(new Lot("Étui pour iPhone", s3BucketUrl + "etui_telephone.jpg", subCategory8, 15.0, 20.0, "active", Identify1));
+            Lot lot21 = lotRepository.save(new Lot("Objectif Sigma 35mm", s3BucketUrl + "objectif_sigma.jpg", subCategory9, 600.0, 650.0, "active", Identify3));
+            Lot lot22 = lotRepository.save(new Lot("Sacoche pour appareil photo", s3BucketUrl + "sacoche_appareil_photo.jpg", subCategory8, 25.0, 30.0, "active", Identify1));
+            Lot lot23 = lotRepository.save(new Lot("Trépied Manfrotto", s3BucketUrl + "trepied_manfrotto.jpg", subCategory9, 150.0, 200.0, "active", Identify3));
+            Lot lot24 = lotRepository.save(new Lot("Écouteurs sans fil", s3BucketUrl + "ecouteur_sansfil.jpg", subCategory8, 50.0, 60.0, "active", Identify1));
+            Lot lot25 = lotRepository.save(new Lot("Stabilisateur DJI Ronin-S", s3BucketUrl + "stabilisateur_ronin.jpg", subCategory9, 400.0, 450.0, "active", Identify2));
+            Lot lot26 = lotRepository.save(new Lot("GoPro Hero 9", s3BucketUrl + "gopro_9.jpg",subCategory9, 350.0, 400.0, "active", Identify3));
+            Lot lot27 = lotRepository.save(new Lot("Étui pour GoPro", s3BucketUrl + "etui_gopro.jpg", subCategory8, 20.0, 25.0, "active", Identify2));
+            Lot lot28 = lotRepository.save(new Lot("Écouteurs Apple AirPods", s3BucketUrl + "AirPods.jpg", subCategory8, 150.0, 180.0, "active", Identify3));
+            Lot lot29 = lotRepository.save(new Lot("Carte mémoire SanDisk", s3BucketUrl + "memorycard_sandisk.jpg", subCategory8, 30.0, 35.0, "active", Identify2));
+            Lot lot30 = lotRepository.save(new Lot("Gimbal pour smartphone", s3BucketUrl + "gimbal_smartphone.jpg", subCategory9, 100.0, 120.0, "active", Identify1));
+            Lot lot31 = lotRepository.save(new Lot("Souris sans fil Logitech", s3BucketUrl + "souris_logitech.jpg", subCategory8, 25.0, 30.0, "active", Identify1));
+            Lot lot32 = lotRepository.save(new Lot("Clé USB 64 Go", s3BucketUrl + "usb_64.jpg", subCategory8, 20.0, 25.0, "active", Identify3));
+            Lot lot33 = lotRepository.save(new Lot("Batterie externe Anker 10000 mAh", s3BucketUrl + "batterie_anker.jpg", subCategory8, 35.0, 40.0, "active", Identify1));
+            Lot lot34 = lotRepository.save(new Lot("Câble Lightning Apple", s3BucketUrl + "lightning_apple.jpg", subCategory8, 15.0, 20.0, "active", Identify2));
 
             enchereRepository.save(new Enchere(new BigDecimal("150.0"), LocalDateTime.now(), lot1, Identify2));
             enchereRepository.save(new Enchere(new BigDecimal("200.0"), LocalDateTime.now(), lot1, Identify3));
